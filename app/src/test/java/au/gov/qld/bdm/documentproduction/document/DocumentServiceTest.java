90d6c363e6e76e7d6899426436d4cf875bffa490
Steven Baker <steven.baker@justice.qld.gov.au>
2021-10-07 09:21:14 +1000

    strengthen test around document content

--- app/src/test/java/au/gov/qld/bdm/documentproduction/document/DocumentServiceTest.java
+++ app/src/test/java/au/gov/qld/bdm/documentproduction/document/DocumentServiceTest.java
@@ -1,18 +1,30 @@
 package au.gov.qld.bdm.documentproduction.document;
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 
+import java.awt.image.RenderedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -58,7 +70,7 @@ public class DocumentServiceTest {
 	}
 	
 	@Test
-	public void shouldCreateAndListByAgency() {
+	public void shouldCreateAndListByAgency() throws IOException {
 		String signatureKeyAlias = RandomStringUtils.randomAlphabetic(10);
 		String kmsId = RandomStringUtils.randomAlphabetic(10);
 		String certificate = RandomStringUtils.randomAlphabetic(10);
@@ -84,12 +96,13 @@ public class DocumentServiceTest {
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		service.produce(credential, documentId, templateModel, DocumentOutputFormat.PDF, os);
 		assertThat(os.toByteArray().length, greaterThan(0));
+		verifyHasText(PDDocument.load(os.toByteArray()));
 	}
 	
 	@Test
 	public void shouldCreateDocumentWithSignedQRCode() throws IOException {
 		String signatureKeyAlias = RandomStringUtils.randomAlphabetic(10);
-		String kmsId = "395a8416-f32b-4613-9401-c487af1f8215";
+		String kmsId = RandomStringUtils.randomAlphabetic(10);
 		String certificate = RandomStringUtils.randomAlphabetic(10);
 		String timestampEndpoint = RandomStringUtils.randomAlphabetic(10);
 		signatureKeyService.save(credential, signatureKeyAlias, kmsId, certificate, timestampEndpoint);
@@ -98,8 +111,8 @@ public class DocumentServiceTest {
 		signatureService.save(credential, signatureAlias, signatureKeyAlias, 1, "reason template", "signatory template", "location template", "contact info template");
 		
 		String templateAlias = RandomStringUtils.randomAlphabetic(10);
-		String content = "<html>test <img src='" + signatureKeyAlias + "' type='signedqrcode' width='250' height='250' /></html>";
-		templateService.save(credential, templateAlias, content);
+		String template = "<html>test ${templateModel['vara']} <br/><img src='" + signatureKeyAlias + "' type='signedqrcode' width='250' height='250' /></html>";
+		templateService.save(credential, templateAlias, template);
 		
 		Map<String, String> templateModel = new HashMap<>();
 		templateModel.put("vara", "a");
@@ -107,13 +120,16 @@ public class DocumentServiceTest {
 		
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		service.produce(credential, documentId, templateModel, DocumentOutputFormat.PDF, os);
+		
 		FileUtils.writeByteArrayToFile(new File(System.getProperty("java.io.tmpdir") + File.separator + "testsignedqr.pdf"), os.toByteArray());
+		verifyHasText(PDDocument.load(os.toByteArray()));
+		verifyHasImage(PDDocument.load(os.toByteArray()));
 	}
 	
 	@Test
 	public void shouldCreateDocumentWithQRCode() throws IOException {
 		String templateAlias = RandomStringUtils.randomAlphabetic(10);
-		String content = "<html>test <img src='some data' type='qrcode' width='250' height='250' /></html>";
+		String content = "<html>test ${templateModel['vara']} <br/><img src='some data' type='qrcode' width='250' height='250' /></html>";
 		templateService.save(credential, templateAlias, content);
 		
 		Map<String, String> templateModel = new HashMap<>();
@@ -122,7 +138,49 @@ public class DocumentServiceTest {
 		
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		service.produce(credential, documentId, templateModel, DocumentOutputFormat.PDF, os);
+
 		FileUtils.writeByteArrayToFile(new File(System.getProperty("java.io.tmpdir") + File.separator + "testqr.pdf"), os.toByteArray());
+		verifyHasText(PDDocument.load(os.toByteArray()));
+		verifyHasImage(PDDocument.load(os.toByteArray()));
+	}
+	
+	private List<RenderedImage> getImagesFromPDF(PDDocument document) throws IOException {
+		List<RenderedImage> images = new ArrayList<>();
+		for (PDPage page : document.getPages()) {
+			images.addAll(getImagesFromResources(page.getResources()));
+		}
+
+		return images;
+	}
+
+	private List<RenderedImage> getImagesFromResources(PDResources resources) throws IOException {
+		List<RenderedImage> images = new ArrayList<>();
+		for (COSName xObjectName : resources.getXObjectNames()) {
+			PDXObject xObject = resources.getXObject(xObjectName);
+	
+			if (xObject instanceof PDFormXObject) {
+				images.addAll(getImagesFromResources(((PDFormXObject) xObject).getResources()));
+			} else if (xObject instanceof PDImageXObject) {
+				images.add(((PDImageXObject) xObject).getImage());
+			}
+		}
+	
+		return images;
+	}
+	
+	private void verifyHasImage(PDDocument pdDocument) throws IOException {
+		List<RenderedImage> imagesFromPDF = getImagesFromPDF(pdDocument);
+		assertThat(imagesFromPDF.size(), is(1));
+		assertThat(imagesFromPDF.get(0).getWidth(), is(125));
+		assertThat(imagesFromPDF.get(0).getHeight(), is(125));
+		pdDocument.close();
+	}
+
+	private void verifyHasText(PDDocument pdDocument) throws IOException {
+		PDFTextStripper pdfStripper = new PDFTextStripper();
+		String pdfContent = pdfStripper.getText(pdDocument);
+		assertThat(pdfContent, containsString("test a"));
+		pdDocument.close();
 	}
 }
 
