a24e70a97e1ea423067d72b2be2feca06e6a29fa
Steven Baker <steven.baker@justice.qld.gov.au>
2021-10-06 12:11:05 +1000

    Adding support for QR and signed QR generation

--- app/src/main/java/au/gov/qld/bdm/documentproduction/document/BarcodeElementFactory.java
+++ app/src/main/java/au/gov/qld/bdm/documentproduction/document/BarcodeElementFactory.java
@@ -1,10 +1,17 @@
 package au.gov.qld.bdm.documentproduction.document;
 
 import java.awt.Color;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+import java.util.zip.Deflater;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.io.IOUtils;
+import org.bouncycastle.operator.ContentSigner;
+import org.joda.time.LocalDate;
 import org.w3c.dom.Element;
 import org.xhtmlrenderer.extend.FSImage;
 import org.xhtmlrenderer.extend.ReplacedElement;
@@ -20,36 +27,117 @@ import com.lowagie.text.BadElementException;
 import com.lowagie.text.Image;
 import com.lowagie.text.pdf.Barcode128;
 
+import au.gov.qld.bdm.documentproduction.document.entity.Document;
+import au.gov.qld.bdm.documentproduction.sign.ContentSignerFactory;
+import au.gov.qld.bdm.documentproduction.signaturekey.SignatureKeyService;
+import au.gov.qld.bdm.documentproduction.signaturekey.entity.SignatureKey;
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
+import nl.minvws.encoding.Base45;
+
 public class BarcodeElementFactory extends ITextReplacedElementFactory {
-	private static final Logger LOG = LoggerFactory.getLogger(BarcodeElementFactory.class);
-	
+	private static final String SIGNED_QRCODE_VERSION = "1.0.0";
 	public static final String IMG_TYPE_ATTRIBUTE = "type";
 	public static final String BARCODE_IMG_TYPE = "barcode";
-	public static final String BARCODE_ELEMENT_TAG = "img";
-	public static final String BARCODE_NUMBER_ATTRIBUTE = "src";
+	public static final String QRCODE_IMG_TYPE = "qrcode";
+	public static final String SIGNED_QRCODE_IMG_TYPE = "signedqrcode";
+	public static final String IMG_TAG = "img";
+	public static final String SRC_ATTRIBUTE = "src";
+	private final Document document;
+	private final Map<String, String> templateModel;
+	private final SignatureKeyService signatureKeyService;
+	private final ContentSignerFactory contentSignerFactory;
 	
-	public BarcodeElementFactory(ITextOutputDevice outputDevice) {
+	public BarcodeElementFactory(ITextOutputDevice outputDevice, Document document, Map<String, String> templateModel, SignatureKeyService signatureKeyService,
+			ContentSignerFactory contentSignerFactory) {
 		super(outputDevice);
+		this.document = document;
+		this.templateModel = templateModel;
+		this.signatureKeyService = signatureKeyService;
+		this.contentSignerFactory = contentSignerFactory;
 	}
 
 	@Override
 	public ReplacedElement createReplacedElement(LayoutContext c, BlockBox box, UserAgentCallback uac, int cssWidth, int cssHeight) {
 		Element element = box.getElement();
-		if (!BARCODE_ELEMENT_TAG.equals(element.getNodeName()) || !BARCODE_IMG_TYPE.equals(element.getAttribute(IMG_TYPE_ATTRIBUTE))) {
-			return super.createReplacedElement(c, box, uac, cssWidth, cssHeight);
+		if (!IMG_TAG.equals(element.getNodeName())) {
+			return super.createReplacedElement(c, box, uac, cssWidth, cssHeight);	
+		}
+		
+		if (BARCODE_IMG_TYPE.equals(element.getAttribute(IMG_TYPE_ATTRIBUTE))) {
+			return createBarcode128(cssWidth, cssHeight, element.getAttribute(SRC_ATTRIBUTE));
+		}
+		
+		if (QRCODE_IMG_TYPE.equals(element.getAttribute(IMG_TYPE_ATTRIBUTE))) {
+			return createQRCode(cssWidth, cssHeight, element.getAttribute(SRC_ATTRIBUTE));
+		}
+		
+		if (SIGNED_QRCODE_IMG_TYPE.equals(element.getAttribute(IMG_TYPE_ATTRIBUTE))) {
+			Optional<SignatureKey> keyForAlias = signatureKeyService.findKeyForAlias(document.getAgency(), element.getAttribute(SRC_ATTRIBUTE));
+			if (!keyForAlias.isPresent()) {
+				throw new IllegalArgumentException("No signature key available for that src");
+			}
+			
+			QRContent qrContent = new QRContent();
+			qrContent.setF(new TreeMap<>(templateModel));
+			qrContent.setKId(keyForAlias.get().getAlias() + ":" + keyForAlias.get().getVersion());
+			qrContent.setDId(document.getId());
+			qrContent.setVer(SIGNED_QRCODE_VERSION);
+			qrContent.setCDate(new LocalDate().toString("yyyy-MM-dd"));
+			try {
+				ContentSigner contentSigner = contentSignerFactory.create(keyForAlias.get());
+				IOUtils.write(qrContent.getSignatureContent(), contentSigner.getOutputStream(), StandardCharsets.UTF_8);
+				String encodedSignature = Base45.getEncoder().encodeToString(contentSigner.getSignature());
+				qrContent.setSig(encodedSignature);
+				return createQRCode(cssWidth, cssHeight, compress(qrContent.getAllContent().getBytes(StandardCharsets.UTF_8)));
+			} catch (IOException e) {
+				throw new IllegalStateException(e.getMessage(), e);
+			}
 		}
 
-		Barcode128 barcode = new Barcode128();
-		barcode.setCode(element.getAttribute(BARCODE_NUMBER_ATTRIBUTE));
-		LOG.debug("Generating barcode for: {}", element.getTextContent());
+		return super.createReplacedElement(c, box, uac, cssWidth, cssHeight);
+	}
+	
+	private String compress(byte[] data) throws IOException {
+		Deflater deflater = new Deflater();
+		deflater.setInput(data);
+
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+		deflater.finish();
+		byte[] buffer = new byte[1024];
+		while (!deflater.finished()) {
+			int count = deflater.deflate(buffer);
+			outputStream.write(buffer, 0, count);
+		}
+		outputStream.close();
+		return Base45.getEncoder().encodeToString(outputStream.toByteArray());
+	}
+
+	private ReplacedElement createQRCode(int width, int height, String content) {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		QRCode.from(content).to(ImageType.PNG).writeTo(os);
 		try {
-			FSImage fsImage = new ITextFSImage(Image.getInstance(barcode.createAwtImage(Color.BLACK, Color.WHITE), Color.WHITE));
-			if (cssWidth > 0 && cssHeight > 0) {
-	            fsImage.scale(cssWidth, cssHeight);
-	          }
-	          return new ITextImageElement(fsImage);
+			return convertToScaledITextImage(Image.getInstance(os.toByteArray()), width, height);
 		} catch (BadElementException | IOException e) {
 			throw new IllegalArgumentException(e.getMessage(), e);
 		}
+	}
+
+	private ReplacedElement createBarcode128(int width, int height, String content) {
+		Barcode128 barcode = new Barcode128();
+		barcode.setCode(content);
+		try {
+			return convertToScaledITextImage(Image.getInstance(barcode.createAwtImage(Color.BLACK, Color.WHITE), Color.WHITE), width, height);
+		} catch (BadElementException | IOException e) {
+			throw new IllegalArgumentException(e.getMessage(), e);
+		}
+	}
+	
+	private ReplacedElement convertToScaledITextImage(Image image, int width, int height) {
+		FSImage fsImage = new ITextFSImage(image);
+		if (width > 0 && height > 0) {
+            fsImage.scale(width, height);
+        }
+        return new ITextImageElement(fsImage);
 	}
 }
---
+++ app/src/main/java/au/gov/qld/bdm/documentproduction/document/QRContent.java
@@ -0,0 +1,71 @@
+package au.gov.qld.bdm.documentproduction.document;
+
+import java.util.SortedMap;
+
+import com.google.gson.Gson;
+
+public class QRContent {
+
+	private SortedMap<String, String> f;
+	private String sig;
+	private String kid;
+	private String dId;
+	private String ver;
+	private String cdate;
+
+	public SortedMap<String, String> getF() {
+		return f;
+	}
+
+	public String getSig() {
+		return sig;
+	}
+
+	public String getKId() {
+		return kid;
+	}
+
+	public String getDId() {
+		return dId;
+	}
+
+	public void setF(SortedMap<String, String> f) {
+		this.f = f;
+	}
+
+	public void setSig(String sig) {
+		this.sig = sig;
+	}
+
+	public void setKId(String kId) {
+		this.kid = kId;
+	}
+
+	public String getSignatureContent() {
+		return new Gson().toJson(f);
+	}
+
+	public String getAllContent() {
+		return new Gson().toJson(this);
+	}
+
+	public void setDId(String documentId) {
+		this.dId = documentId;
+	}
+
+	public void setVer(String ver) {
+		this.ver = ver;
+	}
+	
+	public String getVer() {
+		return ver;
+	}
+
+	public void setCDate(String cDate) {
+		this.cdate = cDate;
+	}
+	
+	public String getCDate() {
+		return cdate;
+	}
+}
