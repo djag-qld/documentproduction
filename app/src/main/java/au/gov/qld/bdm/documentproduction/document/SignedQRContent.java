11c8a09c06161a0b8611a96cebaae7ac2413ff36
Steven Baker <steven.baker@justice.qld.gov.au>
2021-10-11 13:59:42 +1000

    switching to jackson objectmapper to handle escaping better

--- app/src/main/java/au/gov/qld/bdm/documentproduction/document/SignedQRContent.java
+++ app/src/main/java/au/gov/qld/bdm/documentproduction/document/SignedQRContent.java
@@ -7,40 +7,36 @@ import java.util.TreeMap;
 
 import org.joda.time.LocalDate;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SignedQRContent {
-
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+	
+	@JsonIgnore
 	private static final String VERSION = "1.1.0";
 
+	@JsonProperty(index = 0, value = "f")
 	private final SortedMap<String, String> f;
+	@JsonProperty(index = 1, value = "dId")
 	private final String dId;
+	@JsonProperty(index = 2, value = "ver")
 	private final String ver;
+	@JsonProperty(index = 3, value = "cdate")
 	private final String cdate;
+	@JsonProperty(index = 4, value = "sig")
 	private String sig;
+	@JsonProperty(index = 5, value = "kid")
 	private String kid;
 
+	@JsonIgnore
 	public SignedQRContent(String dId, Date createdDate, Map<String, String> f) {
 		this.dId = dId;
 		this.cdate = new LocalDate(createdDate).toString("yyyy-MM-dd");
 		this.f = new TreeMap<>(f);
 		this.ver = VERSION;
-	}
-
-	public SortedMap<String, String> getF() {
-		return f;
-	}
-
-	public String getSig() {
-		return sig;
-	}
-
-	public String getKId() {
-		return kid;
-	}
-
-	public String getDId() {
-		return dId;
 	}
 
 	public void setSig(String sig) {
@@ -51,15 +47,22 @@ public class SignedQRContent {
 		this.kid = kId;
 	}
 
+	@JsonIgnore
 	public String getAllContent() {
-		return new Gson().toJson(this);
+		try {
+			return MAPPER.writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			throw new IllegalStateException(e.getMessage(), e);
+		}
 	}
 
-	public String getVer() {
-		return ver;
+	@JsonIgnore
+	public Map<String, String> getF() {
+		return f;
 	}
-	
-	public String getCDate() {
-		return cdate;
+
+	@JsonIgnore
+	public String getSig() {
+		return sig;
 	}
 }
---
+++ app/src/test/java/au/gov/qld/bdm/documentproduction/document/SignedQRContentTest.java
@@ -0,0 +1,29 @@
+package au.gov.qld.bdm.documentproduction.document;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Date;
+import java.util.Map;
+
+import org.joda.time.LocalDate;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+public class SignedQRContentTest {
+
+	private static final String DOCUMENT_ID = "some document id";
+	private static final Date CREATED = new LocalDate(2021, 10, 11).toDate();
+	private static final Map<String, String> FIELDS = ImmutableMap.of("field", "field's \"value\"");
+	private static final String KEY_ID = "some key id";
+	private static final String SIGNATURE = "some signature";
+
+	@Test
+	public void shouldReturnJsonInConsistentOrderToAllowReliableSignatureVerificationOfRawData() {
+		SignedQRContent content = new SignedQRContent(DOCUMENT_ID, CREATED, FIELDS);
+		content.setKId(KEY_ID);
+		content.setSig(SIGNATURE);
+		assertThat(content.getAllContent(), is("{\"f\":{\"field\":\"field's \\\"value\\\"\"},\"dId\":\"some document id\",\"ver\":\"1.1.0\",\"cdate\":\"2021-10-11\",\"sig\":\"some signature\",\"kid\":\"some key id\"}"));
+	}
+}
